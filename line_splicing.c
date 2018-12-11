7e10b7a547cff32f73b50507917c09280587b8d3
Shruti Jain <shruti.jain1@learner.manipal.edu>
2018-12-11 14:23:31 +0530

    line_splicing.c
    Signed-off-by: Shruti Jain <shruti.jain1@learner.manipal.edu>

---
+++ line_splicing.c
@@ -0,0 +1,12 @@
+//https://www.geeksforgeeks.org/line-splicing-in-c-cpp/
+//
+#include <stdio.h> 
+
+int main() 
+{ 
+	// Line Splicing\ 
+	printf("Hello GFG\n"); 
+	printf("welcome"); 
+	return (0); 
+} 
+
